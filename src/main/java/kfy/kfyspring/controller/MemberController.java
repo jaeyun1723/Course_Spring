d4c47648dc01ffc95076838a24ca104590372cb9
kimjaeyun <jaeyun1723@naver.com>
2023-12-12 21:03:59 +0900

    Feat. section5

--- src/main/java/kfy/kfyspring/controller/MemberController.java
+++ src/main/java/kfy/kfyspring/controller/MemberController.java
@@ -1,18 +1,44 @@
 package kfy.kfyspring.controller;
 
+import kfy.kfyspring.domain.Member;
 import kfy.kfyspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 스프링 컨테이너가 컨트롤러 관리
 public class MemberController {
 
     private final MemberService memberService;
 
-
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+        Member member=  new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
 }
